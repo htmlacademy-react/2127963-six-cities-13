eb1919bc3ab158612c56eaff628d14444b7e235c
Elvira <elveechernenko@yandex.ru>
2023-07-13 14:02:58 +0300

    Добавляет маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Main from '../../pages/main/main';
+import Offer from '../../pages/offer/offer';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import { AppRoute } from '../../const';
 
 type AppProps = {
   offersNumber: number;
@@ -8,7 +13,26 @@ type AppProps = {
 
 function App({offersNumber, userName, favoritesNumber}: AppProps): JSX.Element {
   return (
-    <Main offersNumber={offersNumber} userName={userName} favoritesNumber={favoritesNumber}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main offersNumber={offersNumber} userName={userName} favoritesNumber={favoritesNumber} />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,14 +1,21 @@
-enum Setting {
-  OffersNumber = 5,
-  UserName = 'Oliver.conner@gmail.com',
-  FavoritesNumber = 5
-}
+const Setting = {
+  OffersNumber: 5,
+  UserName: 'Oliver.conner@gmail.com',
+  FavoritesNumber: 5
+} as const;
 
-enum Accomodation {
-  'apartment' = 'Apartment',
-  'room' = 'Private Room',
-  'house' = 'House',
-  'hotel' = 'Hotel'
-}
+const AppRoute = {
+  Main: '/',
+  Login: '/login',
+  Favorites: '/favorites',
+  Offer: '/offer'
+} as const;
 
-export { Setting, Accomodation };
+const Accomodation = {
+  'apartment': 'Apartment',
+  'room': 'Private Room',
+  'house':'House',
+  'hotel':'Hotel'
+} as const;
+
+export { Setting, AppRoute, Accomodation };
